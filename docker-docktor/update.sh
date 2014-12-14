4ed37ab4c10c41fff9d2109c349adde0af4baaea
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2014-12-14 11:19:32 +0100

    fix : docktor production startup

--- docker-docktor/update.sh
+++ docker-docktor/update.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export NODE_ENV=production
-
-cd /opt/docktor/docktor && \
+# need to set NODE_ENV to dev before grunt build 
+# to have file application.min.* (task uglify:production and cssmin:combine)
+(export NODE_ENV=development && \
+	echo "---------------> Updating with NODE_ENV=${NODE_ENV} and user `id`" && \
+	cd /opt/docktor/docktor && \
+	echo '---------------> Pulling...'  && \
     git pull && \
+    echo '---------------> npm install...' && \
     npm install --config.interactive=false && \
-    grunt build
+    echo '---------------> Building...' && \
+    grunt build && \
+    echo '---------------> End Update')
